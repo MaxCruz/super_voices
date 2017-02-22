686e4d5e4991deb90418ddf157ad660848a83f80
Max Cruz <max.raul@gmail.com>
2017-02-22 02:23:43 -0500

    Validates presence of the foreign keys

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,4 +1,6 @@
 class Contest < ApplicationRecord
     belongs_to :administrator
     has_many :voices
+
+    validates :administrator_id, :presence => true
 end
--- app/models/voice.rb
+++ app/models/voice.rb
@@ -1,3 +1,5 @@
 class Voice < ApplicationRecord
     belongs_to :contest
+
+    validates :contest_id, :presence => true
 end
