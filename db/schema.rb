498d3a9704430ede94fca82a0bbca5c0c6e91921
cami7ord <cami7ord@gmail.com>
2017-02-26 03:04:51 -0500

    Job scheduled to run every 1 minute using Crono gem, before running the server, must run in another terminal the command: bundle exec crono RAILS_ENV=development on the root directory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225230414) do
+ActiveRecord::Schema.define(version: 20170226074646) do
 
   create_table "administrators", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,16 @@ ActiveRecord::Schema.define(version: 20170225230414) do
     t.integer  "administrator_id"
     t.text     "script"
     t.text     "recomendations"
+  end
+
+  create_table "crono_jobs", force: :cascade do |t|
+    t.string   "job_id",                               null: false
+    t.text     "log",               limit: 1073741823
+    t.datetime "last_performed_at"
+    t.boolean  "healthy"
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.index ["job_id"], name: "index_crono_jobs_on_job_id", unique: true
   end
 
   create_table "voices", force: :cascade do |t|
