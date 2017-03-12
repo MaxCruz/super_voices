677c3130f170f57180fcd412727fdc921cb0186a
Max Cruz <max.raul@gmail.com>
2017-03-12 17:18:07 -0500

    Migrations

--- db/migrate/20170312221027_contests_add_missing_columns.rb
+++ db/migrate/20170312221027_contests_add_missing_columns.rb
@@ -2,6 +2,6 @@ class ContestsAddMissingColumns < ActiveRecord::Migration[5.0]
   def change
       add_column :contests, :script, :text
       add_column :contests, :recomendations, :text
-      change_column :contests, :reward, :numeric
+      change_column :contests, :reward, 'integer USING CAST(reward AS integer)'
   end
 end
