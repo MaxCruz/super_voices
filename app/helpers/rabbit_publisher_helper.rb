7e6556aed93a8e0fc1287c97c71907610cb498d4
Max Cruz <maxc@blanclabs.com>
2017-04-29 10:37:56 -0500

    RabbitMQ queue

--- app/helpers/rabbit_publisher_helper.rb
+++ app/helpers/rabbit_publisher_helper.rb
@@ -5,7 +5,7 @@ module RabbitPublisherHelper
 	def connect
 		@connection = Bunny.new(ENV['RABBITMQ_BIGWIG_TX_URL'])
 		@connection.start
-		channel = connection.create_channel
+		channel = @connection.create_channel
 		@queue = channel.queue(ENV['RABBITMQ_QUEUE'], auto_delete => true)
 		@exchange = channel.default_exchange
 	end
