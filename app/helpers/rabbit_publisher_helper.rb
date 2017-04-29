0aa3d6a8730581eaedac27c6263cf2b9cb14072c
Max Cruz <maxc@blanclabs.com>
2017-04-29 10:23:54 -0500

    RabbitMQ queue

--- app/helpers/rabbit_publisher_helper.rb
+++ app/helpers/rabbit_publisher_helper.rb
@@ -3,22 +3,19 @@ require 'bunny'
 module RabbitPublisherHelper
 
 	def connect
-		connection = Bunny.new(ENV['RABBITMQ_BIGWIG_TX_URL'])
+		@connection = Bunny.new(ENV['RABBITMQ_BIGWIG_TX_URL'])
 		connection.start
 		channel = connection.create_channel
-		self.channel = channel
+		@queue = channel.queue(ENV['RABBITMQ_QUEUE'], auto_delete => true)
+		@exchange = channel.default_exchange
 	end
 
-	def publish(exchange_name, message)
-		exchange = channel.fanout(exchange_name, durable: true)
-		exchange.publish(message.to_json)
+	def publish(message)
+		@exchange.publish(message, :routeing_key => @queue.name)
 	end
 
 	def disconect
-
+		@connection.close
 	end
-
-	private
-	attr_accessor :channel
 
 end
