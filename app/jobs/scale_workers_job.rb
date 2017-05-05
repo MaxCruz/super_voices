09872cc0afc1e2aff6a54657bda5176ec5db407d
Max Cruz <maxc@blanclabs.com>
2017-05-05 14:00:44 -0500

    Static workers for demo

--- app/jobs/scale_workers_job.rb
+++ app/jobs/scale_workers_job.rb
@@ -4,14 +4,14 @@ class ScaleWorkerJob < ActiveJob::Base
 	include HerokuHelper
 
 	def perform
-		min = ENV["WORKERS_MIN"].to_i
-		max = ENV["WORKERS_MAX"].to_i
-		limit = ENV["WORKERS_JOB_LIMIT"].to_i
-		increment = ENV["WORKERS_INCREMENT"].to_i
+		min = 1
+		max = 6
+		limit = 10
+		increment = 1
 		rmq_connect
 		heroku_connect
-		messages = rmq_count_messages.to_i
-		consumers = rmq_count_consumers.to_i
+		messages = rmq_count_messages
+		consumers = rmq_count_consumers
 		puts "Current status: #{messages} queued messages for #{consumers} consumers"
 		if messages >= limit && consumers < max
 			puts "Increase one instance"
